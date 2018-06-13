72bdc44dafea76071f389341b60ff26f838b85cb
Kin <zhuangxuxin@hotmail.com>
2018-06-13 13:26:28 +0800

    add my fragment base code

--- app/src/main/java/com/lichkin/application/fragments/MyFragment.java
+++ app/src/main/java/com/lichkin/application/fragments/MyFragment.java
@@ -6,8 +6,15 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
 
 import com.lichkin.app.android.demo.R;
+import com.lichkin.application.activities.AboutActivity;
+import com.lichkin.framework.app.android.beans.LKDynamicButton;
+import com.lichkin.framework.app.android.utils.LKDynamicButtonUtils;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * 我的页面
@@ -15,9 +22,23 @@ import com.lichkin.app.android.demo.R;
  */
 public class MyFragment extends Fragment {
 
+    /** 当前视图 */
+    private View view;
+
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.my_fragment, container, false);
+        //填充页面
+        view = inflater.inflate(R.layout.my_fragment, container, false);
+
+        //初始化按钮栏
+        List<LKDynamicButton> btns = new ArrayList<>();
+        btns.add(new LKDynamicButton(R.drawable.btn_score, R.string.title_score, new ScoreFragment(), getFragmentManager()));
+        btns.add(new LKDynamicButton(R.drawable.btn_feedback, R.string.title_feedback, new FeedbackFragment(), getFragmentManager()));
+        btns.add(new LKDynamicButton(R.drawable.btn_about, R.string.title_about, AboutActivity.class));
+        LKDynamicButtonUtils.inflate((LinearLayout) view.findViewById(R.id.btns), btns, 4, 1);
+
+        //返回视图
+        return view;
     }
 
 }
